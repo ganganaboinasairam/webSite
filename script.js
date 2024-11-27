87a7dc74167c0a9ff2747d0955c0fe23b66e631a
“ganganaboinasairam” <ganganaboinasairam@gmail.com>
2024-11-27 23:44:37 +0530

    added files from the old host

--- script.js
+++ script.js
@@ -2,4 +2,9 @@
 document.addEventListener("DOMContentLoaded", () => {
     console.log("Portfolio Website Loaded");
     // Additional interactivity can be added here.
+	
+	if (window.location.hash) {
+        window.history.replaceState(null, null, window.location.href.split("#")[0]);
+    }
 });
+
